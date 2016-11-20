169afc2359963244020d81748be82be86d68f632
Tyson Burghardt <tb@manchru.net>
2016-11-20 14:39:29 -0500

    got it right, added us2016 and trump plans

--- tax.py
+++ tax.py
@@ -13,16 +13,27 @@ class taxdata:
 
 def calctax(tdata, incomes):
     nn = np.vstack((np.copy(incomes)-tdata.deduction, np.zeros(np.size(incomes))))
-    d = np.empty(nn[0,:].shape)
+    dr = np.zeros(nn[0,:].shape)
     for lim, mrate  in tdata.brackets:
-        d = [ max(i - lim, 0.0) for i in nn[0,:]]
-        nn[1,:] += d*mrate
-        nn[0,:] -=d
+        dr = np.array([ max(i - lim, 0.0) for i in nn[0,:]])
+        nn[1,:] += dr*mrate
+        nn[0,:] -= dr
     return nn[1,:]
 
 
-crapola = taxdata(  brackets = [ (0.,0.1), (30000., 0.25), (100000., 0.33)],
+simple_sample = taxdata(  brackets = [ (0.,0.1), (30000., 0.25), (100000., 0.33)],
                     deduction = 10000 )
 
+
+
+us2016 = taxdata(deduction=12600., 
+                 brackets = [ 
+                     (466950., 0.396), (413350., 0.35),
+                     (231450., 0.33), (151900., 0.28),
+                     (75300., 0.25), (18550., 0.15), (0, 0.1)])
+
+trump = taxdata(deduction=30000.,
+            brackets = [
+                (225000., 0.33), (75000., 0.25), (0., 0.12) ])
 
 
