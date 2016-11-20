08d44eca8013ed66c86a5b54653e98ecbe938825
psammetichus <tyson.burghardt@gmail.com>
2016-11-20 08:09:20 -0500

    fixed

--- tax.py
+++ tax.py
@@ -8,21 +8,20 @@ HOH = 4
 
 @attr.s
 class taxdata:
-    tclass = attr.ib(default = SING)
     brackets  = attr.ib(default = [ (0.0, 0.25)])
     deduction = attr.ib(default = 0)
 
 def calctax(tdata, incomes):
     nn = np.vstack((np.copy(incomes)-tdata.deduction, np.zeros(np.size(incomes))))
+    d = np.empty(nn[0,:].shape)
     for lim, mrate  in tdata.brackets:
-        d = np.max( np.vstack( (nn[0,:] -lim, np.zeros_like(nn[0:]))), axis=0)
+        d = [ max(i - lim, 0.0) for i in nn[0,:]]
         nn[1,:] += d*mrate
         nn[0,:] -=d
     return nn[1,:]
 
 
-crapola = taxdata(  tclass=SING,
-                    brackets = [ (0.,0.1), (30000., 0.25), (100000., 0.33)],
+crapola = taxdata(  brackets = [ (0.,0.1), (30000., 0.25), (100000., 0.33)],
                     deduction = 10000 )
 
 
